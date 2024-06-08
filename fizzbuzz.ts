2348ad6d5b436d973f8ef23dd480fafcc570ce50
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2024-06-08 17:21:52 -0400

    test

---
+++ fizzbuzz.ts
@@ -0,0 +1,17 @@
+// classic fizzbuzz
+//
+//
+let fizzMap = new Map<number, string>([
+	[3, "Fizz"],
+	[5, "Buzz"]
+])
+function fizzbuzz(n: number):string[] {
+	let result: string[] = []
+	let mod: number = 0
+	for (let i = 1; i <= n.length; ++i){
+		mod = 4
+		switch(mod){
+			case 
+		}
+	}
+}
